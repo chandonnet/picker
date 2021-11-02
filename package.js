d03a1a60a529eb7972e8671fd7f2558df827b56d
chandonnet <franki_football@hotmail.com>
2021-11-02 11:54:23 -0400

    compat 2.3

--- package.js
+++ package.js
@@ -1,28 +1,21 @@
 Package.describe({
-  name: 'communitypackages:picker',
+  name: 'fchandonnet:picker',
   summary: 'Server Side Router for Meteor',
-  version: '1.1.0',
-  git: 'https://github.com/Meteor-Community-Packages/picker.git',
+  version: '1.1.6',
+  git: 'https://github.com/chandonnet/picker',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'path-to-regexp': '6.1.0'
+  'path-to-regexp': '6.2.0'
 });
 
 function configurePackage(api) {
-  api.versionsFrom('1.9');
+  api.versionsFrom('2.3');
   api.use(['webapp', 'ecmascript', 'url'], 'server');
 }
 
 Package.onUse(function(api) {
   configurePackage(api);
   api.mainModule('lib/instance.js', 'server');
-});
-
-Package.onTest(function(api) {
-  configurePackage(api);
-  api.use('communitypackages:picker', 'server');
-  api.use(['tinytest', 'http', 'random'], 'server');
-  api.mainModule('test/instance.js', 'server');
 });
